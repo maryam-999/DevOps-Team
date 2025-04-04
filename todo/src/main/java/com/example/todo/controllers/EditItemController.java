db7d9cfa9536a4a24ad960d30aa71b7543febdc2
Maryam <boufousmaryam287@gmail.com>
2025-04-04 12:21:14 +0000

    ajout des entities

--- todo/src/main/java/com/example/todo/controllers/EditItemController.java
+++ todo/src/main/java/com/example/todo/controllers/EditItemController.java
@@ -1,4 +1,6 @@
 package com.example.todo.controllers;
 
 public class EditItemController {
+
+
 }
--- todo/src/main/java/com/example/todo/entities/ToDoElement.java
+++ todo/src/main/java/com/example/todo/entities/ToDoElement.java
@@ -1,5 +1,52 @@
 package com.example.todo.entities;
 
-public class ToDoElement {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
+import java.io.Serializable;
+
+@Entity
+@Table(name = "elements")
+public class ToDoElement implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "toDoList_Id")
+    private ToDoList toDoList;
+    @NotBlank(message = "Name is required")
+    private String name;
+    private Boolean isDone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ToDoList getToDoList() {
+        return toDoList;
+    }
+
+    public void setToDoList(ToDoList toDoList) {
+        this.toDoList = toDoList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getIsDone() {
+        return isDone;
+    }
+
+    public void setIsDone(Boolean done) {
+        isDone = done;
+    }
 }
