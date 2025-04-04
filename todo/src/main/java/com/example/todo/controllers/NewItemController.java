7b189e4701bcce4821aced2839a19d22f4b2be19
maryem <maryemymil@gmail.com>
2025-04-04 13:24:11 +0000

    US-07: Création d'une liste des élèments

--- todo/src/main/java/com/example/todo/controllers/NewItemController.java
+++ todo/src/main/java/com/example/todo/controllers/NewItemController.java
@@ -1,7 +1,35 @@
 package com.example.todo.controllers;
+import com.example.todo.entities.*;
+import com.example.todo.repositories.*;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-
+@Controller
 public class NewItemController {
 
+    @Autowired
+    private ToDoListRepository toDoListRepository;
 
+    @GetMapping("/create-list")
+    public ModelAndView showCreateListPage() {
+        ModelAndView modelAndView = new ModelAndView("create-list-page");
+        modelAndView.addObject("toDoList", new ToDoList());
+        return modelAndView;
+    }
+
+    @PostMapping("/create-new-list")
+    public String createListItem(@Valid ToDoList toDoList, BindingResult result, Model model) {
+
+        ToDoList list = new ToDoList();
+        list.setName(toDoList.getName());
+
+        toDoListRepository.save(list);
+        return "redirect:/";
+    }
 }
