4202210a2199b6391508b59bd4f13eae62dd45e8
maryem <maryemymil@gmail.com>
2025-04-21 21:42:05 +0100

    US-12: Modifier un élément

--- todo/src/main/java/com/example/todo/repositories/ToDoElementRepository.java
+++ todo/src/main/java/com/example/todo/repositories/ToDoElementRepository.java
@@ -2,6 +2,10 @@ package com.example.todo.repositories;
 
 import com.example.todo.entities.ToDoElement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface ToDoElementRepository extends JpaRepository<ToDoElement, Long> {
 }
